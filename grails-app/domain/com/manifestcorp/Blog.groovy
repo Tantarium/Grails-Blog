6966390a3ce7218fd37d528d7280cc84a3d8dcd2
Larry Pohto III <lpohto@manifestcorp.com>
2017-03-15 15:58:12 -0400

    re-initialized project

---
+++ grails-app/domain/com/manifestcorp/Blog.groovy
@@ -0,0 +1,14 @@
+package com.manifestcorp
+
+class Blog {
+
+    String name
+    String note
+    Date dateCreated
+
+    static constraints = {
+        name(blank:false)
+        note(maxSize:1000, widget:'textarea', nullable: true)
+        //This is a comment
+    }
+}
