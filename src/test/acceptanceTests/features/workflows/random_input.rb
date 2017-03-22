38c77cf24e77a20a3cd30cad0b4eca0b1ed9f16d
Larry Pohto III <lpohto@manifestcorp.com>
2017-03-22 10:44:13 -0400

    Made a slight change to the way random input was generated in the ruby acceptance test.

--- src/test/acceptanceTests/features/workflows/random_input.rb
+++ src/test/acceptanceTests/features/workflows/random_input.rb
@@ -4,12 +4,15 @@
 module RandomInput
 
   def random_string(x)
-    (0...x).map { (65 + rand(26)).chr }.join
+    (0...x).map { (65 + rand(26)).chr.downcase }.join
   end
 
   def random_blog_text
-    "#{random_string(50)} #{random_string(50)} #{random_string(50)} #{random_string(50)} #{random_string(50)}
-    #{random_string(50)} #{random_string(50)} #{random_string(50)} #{random_string(50)} #{random_string(50)}"
+    @string_to_return
+    for i in 0..50
+      @string_to_return = "#{@string_to_return} #{random_string(10)}"
+    end
+    @string_to_return
   end
 
 end
