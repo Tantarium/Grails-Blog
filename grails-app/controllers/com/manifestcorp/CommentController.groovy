816e03e79477326c8b965f3e75a4fd1aea2796ac
Larry Pohto III <lpohto@manifestcorp.com>
2017-04-04 10:47:20 -0400

    Removed comment views that were no longer needed and didn't do anything. Cleaned up the Comment Controller to only have the delete method for deleting comments, since other methods were not used at all.

--- grails-app/controllers/com/manifestcorp/CommentController.groovy
+++ grails-app/controllers/com/manifestcorp/CommentController.groovy
@@ -8,44 +8,10 @@ import grails.plugin.springsecurity.annotation.Secured
 @Secured('ROLE_USER')
 class CommentController {
 
-    static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
-
-    def show(Comment comment) {
-        respond comment
-    }
-
-    @Secured('permitAll')
-    def create() {
-        respond new Comment(params)
-    }
-
-    @Transactional
-    @Secured('permitAll')
-    def save(Comment comment) {
-        if (comment == null) {
-            transactionStatus.setRollbackOnly()
-            notFound()
-            return
-        }
-
-        if (comment.hasErrors()) {
-            transactionStatus.setRollbackOnly()
-            respond comment.errors, view:'create'
-            return
-        }
-
-        comment.save flush:true
-
-        redirect controller: 'blog', action: 'show', id: comment.blog.id
-    }
+    static allowedMethods = [delete: "DELETE"]
 
     @Transactional
     def delete(Comment comment) {
-        if (comment == null) {
-            transactionStatus.setRollbackOnly()
-            notFound()
-            return
-        }
 
         comment.delete flush:true
 
@@ -55,16 +21,6 @@ class CommentController {
                 redirect controller: 'Blog', action: 'show', id: comment.blog.id
             }
             '*'{ render status: NO_CONTENT }
-        }
-    }
-
-    protected void notFound() {
-        request.withFormat {
-            form multipartForm {
-                flash.message = message(code: 'default.not.found.message', args: [message(code: 'comment.label', default: 'Comment'), params.id])
-                redirect action: "index", method: "GET"
-            }
-            '*'{ render status: NOT_FOUND }
         }
     }
 }
